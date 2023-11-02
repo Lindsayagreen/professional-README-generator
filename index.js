24e47682e4aca5d10202f88633c8d079c57b5c04
lindsayagreen <lindsgree@gmail.com>
2023-11-02 14:51:36 -0500

    fixed questions and list

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-// const questions = [];
-
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
 
@@ -14,55 +6,87 @@ const fs = require('fs');
 
 // Function call to initialize app
 // init();
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your project title name?',
-      name: 'projectName',
-    },
-    {
-      type: 'input',
-      message: 'What is the projects description?',
-      name: 'description',
-    },
-    {
-      type: 'list',
-      message: 'List table of contents',
-      name: 'contents',
-    },
-    {
-        type: 'input',
-        message: 'Installation',
-        name: 'install',
-    },
-    {
-        type: 'input',
-        message: 'Usage',
-        name: 'uses',
-    },
-    {
-        type: 'input',
-        message: 'License',
-        name: 'licenses',
-    },
-    {
-        type: 'input',
-        message: 'Tests',
-        name: 'test',
-    },
-    {
-        type: 'input',
-        message: 'Questions',
-        name: 'question',
-    },
-])
-  .then(answers => {
-    // console.log(answers.name); 
-    const filename = `${answers.name.toLowerCase().split('').join('')}.json`
+const questions = [
+  {
+    type: "input",
+    message: "What is your project title name?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the projects description?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Installation instructions",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Usage information",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Test Instructions",
+    name: "test",
+  },
+  {
+    type: "list",
+    message: "Choose a license",
+    name: "license",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "None"],
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+];
 
-    fs.writeFile(filename, JSON.stringify(answers), (err) => {
-      err ? console.error(err) :console.log('Success!');
-    })
-  }); 
+inquirer.prompt(questions).then((answers) => {
+  const filename = `${answers.projectName
+    .toLowerCase()
+    .split(" ")
+    .join("")}.json`;
+
+  fs.writeFile(filename, JSON.stringify(answers), (err) => {
+    err ? console.error(err) : console.log("README.md generated successfully!");
+  });
+});
+
+function generateREADME(answers) {
+  return `
+  # ${answers.title}
+  ## Description
+  ${answers.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Installation
+  ${answers.installation}
+  ## Usage
+  ${answers.usage}
+  ## License
+  This project is licensed under the ${answers.license} license.
+  ## Contributing
+  ${answers.contributing}
+  ## Tests
+  ${answers.tests}
+  ## Questions
+  For additional questions, you can reach me at [${answers.email}](mailto:${answers.email}). You can also find me on [GitHub](https://github.com/${answers.github}).
+  `;
+}
